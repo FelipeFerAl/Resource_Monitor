aac9942724e15d4ccf2cf9391924b2060c6d74e4
PaulinaRB <142756900+PaulinaRuizB@users.noreply.github.com>
2025-05-21 15:00:28 -0500

    Update meminfo_manip.c comments and documentation

--- src/meminfo_manip.c
+++ src/meminfo_manip.c
@@ -1,21 +1,25 @@
-#include "meminfo_manip.h"
+#include "meminfo_manip.h"  
 
 void meminfo(float *MemTotal, float *SwapTotal, float *UsedSpace, float *UsedSwap){
-    FILE *fp = fopen("/proc/meminfo", "r");
+    FILE *fp = fopen("/proc/meminfo", "r");  // Abre el archivo del sistema con info de memoria
 
     char line[256];
-    float MemFree, SwapFree;
+    float MemFree, SwapFree;  // Variables auxiliares para calcular uso
+
+    // Recorre el archivo línea por línea buscando etiquetas específicas
     while(fgets(line, sizeof(line), fp)){
-        if(strstr(line, "MemTotal")){
+        if(strstr(line, "MemTotal")){  // RAM total
             sscanf(line, "MemTotal\t:%f", MemTotal);
-        } else if(strstr(line, "SwapTotal")){
+        } else if(strstr(line, "SwapTotal")){  // Swap total
             sscanf(line, "SwapTotal\t:%f", SwapTotal);
-        } else if(strstr(line, "MemFree")){
-	    sscanf(line, "MemFree\t:%f", &MemFree);
-	} else if(strstr(line, "SwapFree")){
-	    sscanf(line, "SwapFree\t:%f", &SwapFree);
-	}
+        } else if(strstr(line, "MemFree")){  // RAM libre
+            sscanf(line, "MemFree\t:%f", &MemFree);
+        } else if(strstr(line, "SwapFree")){  // Swap libre
+            sscanf(line, "SwapFree\t:%f", &SwapFree);
+        }
     }
+
+    // Calcula memoria usada como total - libre
     *UsedSpace = *MemTotal - MemFree;
-    *UsedSwap = *SwapTotal - SwapFree;
+    *UsedSwap  = *SwapTotal - SwapFree;
 }
